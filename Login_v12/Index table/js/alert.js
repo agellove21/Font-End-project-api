5ff5d0f52e5f556393314a5d1ba008cc75873706
agellove21 <48426554+agellove21@users.noreply.github.com>
2019-03-14 19:16:52 +0700

    Nhật

--- Login_v12/Index table/js/alert.js
+++ Login_v12/Index table/js/alert.js
@@ -1,6 +1,31 @@
-document.getElementsByTagName('body')[0].onmousedown = function(e) {
-    if(e.which) {
-        alert("Make by Huỳnh Minh Nhật"); // original click
+var d=new Date();
+  //var day= d.getDay().toString();
+  //var month=d.getMonth().toString();
+  //var year=d.getFullYear().toString();
+    document.getElementById("date").innerHTML =d;
+  var d= date
+  document.getElementById("date").innerHTML = Date();
+$("tr").click(function(e) {
+    if (e.target.type == "checkbox") {
+        e.stopPropagation();
+    } else {
+        var $checkbox = $(this).find(':checkbox');
+        $checkbox.attr('checked', !$checkbox.attr('checked'));
     }
-}
-document.getElementsByTagName('body')[0].onmousedown();
+});
+  function thongbao(){
+   swal({
+    title:"Những sinh viên thực hiện",
+     text:"Huỳnh Minh Nhật , Nguyễn Quốc Bảo, Đặng Quốc Bình,Nguyễn Phúc Thịnh",
+     icon:"info",
+     button:"Xác nhận!"}
+    );
+  }
+    var string = "Huỳnh Minh Nhật - Nguyễn Quốc Bảo - Đặng Quốc Bình - Nguyễn Phúc Thịnh";
+    var str = string.split("");
+    var el = document.getElementById('str');
+    (function animate() {
+    str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+    var running = setTimeout(animate, 90);
+    })();
+  
